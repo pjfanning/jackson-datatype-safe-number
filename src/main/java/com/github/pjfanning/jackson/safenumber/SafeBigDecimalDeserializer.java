8a76d10ee31d6c2101087a246b858b7513224d10
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-24 11:53:31 +0200

    jackson 2.15

--- src/main/java/com/github/pjfanning/jackson/safenumber/SafeBigDecimalDeserializer.java
+++ src/main/java/com/github/pjfanning/jackson/safenumber/SafeBigDecimalDeserializer.java
@@ -1,12 +1,16 @@
 package com.github.pjfanning.jackson.safenumber;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.io.NumberInput;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.github.pjfanning.safenumberparser.ConstraintException;
 import com.github.pjfanning.safenumberparser.SafeBigDecimal;
+import com.github.pjfanning.safenumberparser.SafeNumberParserConfig;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 final class SafeBigDecimalDeserializer extends StdDeserializer<SafeBigDecimal> {
 
@@ -18,8 +22,23 @@ final class SafeBigDecimalDeserializer extends StdDeserializer<SafeBigDecimal> {
 
     @Override
     public SafeBigDecimal deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        final String input = p.getValueAsString();
         try {
-            return new SafeBigDecimal(p.getValueAsString());
+            if (p.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)) {
+                if (input.length() > SafeNumberParserConfig.getBigDecimalMaxLength()) {
+                    throw new ConstraintException(
+                            "Failed to parse SafeBigDecimal because the input is too long; max allowed chars is " +
+                                    SafeNumberParserConfig.getBigDecimalMaxLength());
+                }
+                final BigDecimal bigDecimal = NumberInput.parseBigDecimal(p.getValueAsString(), true);
+                if (Math.abs(bigDecimal.scale()) > SafeNumberParserConfig.getBigDecimalMaxScale()) {
+                    throw new ConstraintException(
+                            "Failed to parse SafeBigDecimal because the scale is too large; max allowed scale is " +
+                                    SafeNumberParserConfig.getBigDecimalMaxScale());
+                }
+                return new SafeBigDecimal(bigDecimal);
+            }
+            return new SafeBigDecimal(input);
         } catch (ConstraintException e) {
             throw new IOException(e);
         }
--- src/main/java/com/github/pjfanning/jackson/safenumber/SafeBigIntegerDeserializer.java
+++ src/main/java/com/github/pjfanning/jackson/safenumber/SafeBigIntegerDeserializer.java
@@ -1,12 +1,17 @@
 package com.github.pjfanning.jackson.safenumber;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.io.NumberInput;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.github.pjfanning.safenumberparser.ConstraintException;
 import com.github.pjfanning.safenumberparser.SafeBigInteger;
+import com.github.pjfanning.safenumberparser.SafeNumberParserConfig;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 final class SafeBigIntegerDeserializer extends StdDeserializer<SafeBigInteger> {
 
@@ -18,8 +23,28 @@ final class SafeBigIntegerDeserializer extends StdDeserializer<SafeBigInteger> {
 
     @Override
     public SafeBigInteger deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        final String input = p.getValueAsString();
         try {
-            return new SafeBigInteger(p.getValueAsString());
+            if (p.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)) {
+                if (input.length() > SafeNumberParserConfig.getBigIntegerMaxLength()) {
+                    throw new ConstraintException(
+                            "Failed to parse SafeBigInteger because the input is too long; max allowed chars is " +
+                                    SafeNumberParserConfig.getBigDecimalMaxLength());
+                }
+                if (SafeNumberParserConfig.isBigIntegerENotationSupported()) {
+                    final BigDecimal bigDecimal = NumberInput.parseBigDecimal(p.getValueAsString(), true);
+                    if (Math.abs(bigDecimal.scale()) > SafeNumberParserConfig.getBigIntegerMaxScale()) {
+                        throw new ConstraintException(
+                                "Failed to parse SafeBigInger because the scale is too large; max allowed scale is " +
+                                        SafeNumberParserConfig.getBigIntegerMaxScale());
+                    }
+                    return SafeNumberParserConfig.isBigIntegerExactConversionRequired()
+                            ? new SafeBigInteger(bigDecimal.toBigIntegerExact())
+                            : new SafeBigInteger(bigDecimal.toBigInteger());
+                }
+                return new SafeBigInteger(NumberInput.parseBigInteger(p.getValueAsString(), true));
+            }
+            return new SafeBigInteger(input);
         } catch (ConstraintException e) {
             throw new IOException(e);
         }
